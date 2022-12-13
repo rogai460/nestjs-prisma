9dc7e96349b12f76383e1aafc6bcaa88539f0e85
rogai460 <115393561+rogai460@users.noreply.github.com>
2022-12-13 10:00:59 +0000

    db

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,17 +1,44 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
-import { EngineerProjectHistory, Prisma } from '@prisma/client';
+import { Project, Prisma } from '@prisma/client';
 
 @Injectable()
 export class ProjectsService {
   constructor(private prisma: PrismaService) {}
 
-  async projects(): Promise<EngineerProjectHistory[]> {
-    return this.prisma.engineerProjectHistory.findMany({
+  async projects(): Promise<Project[]> {
+    const findProject = this.prisma.project.findMany({
       include: {
-        project: true,
-        engineer: true,
+        projectHistory: {
+          include: {
+            engineer: {
+              select: {
+                lastName: true,
+                firstName: true,
+                lastNameKana: true,
+                firstNameKana: true,
+                sex: true,
+                company: true,
+              },
+            },
+          },
+        },
       },
     });
+
+    return (await findProject).map((fp) => ({
+      ...fp,
+      projectHistory: fp.projectHistory.map((ph) => ({
+        startDate: ph.startDate,
+        endDate: ph.endDate,
+        engineerId: ph.engineerId,
+        lastName: ph.engineer.lastName,
+        firstName: ph.engineer.firstName,
+        lastNameKana: ph.engineer.lastNameKana,
+        firstNameKana: ph.engineer.firstNameKana,
+        sex: ph.engineer.sex,
+        company: ph.engineer.company,
+      })),
+    }));
   }
 }
